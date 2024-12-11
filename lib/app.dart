d1029a25b0a45676d23f0c30d5c0ad22c26b25ff
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2024-12-11 19:44:50 +0545

    changes

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:bookit_flutter_project/view/loginView.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:LoginView(),
+    );
+  }
+}
