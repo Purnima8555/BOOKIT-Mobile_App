2b3faac0772dbae5fcd191006827f35f988062a9
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2025-02-13 13:21:45 +0545

    Refactor splash_cubit logic

--- lib/features/splash/presentation/view_model/splash_cubit.dart
+++ lib/features/splash/presentation/view_model/splash_cubit.dart
@@ -1,24 +1,19 @@
-import 'package:bookit_flutter_project/features/auth/presentation/view/login_view.dart';
-import 'package:bookit_flutter_project/features/auth/presentation/view_model/login/login_bloc.dart';
+import 'package:bookit_flutter_project/features/on_boarding/presentation/view/onboarding_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashCubit extends Cubit<void> {
-  SplashCubit(this._loginBloc) : super(null);
-
-  final LoginBloc _loginBloc;
+  SplashCubit() : super(null);
 
   Future<void> init(BuildContext context) async {
     await Future.delayed(const Duration(seconds: 2), () async {
 
+      // Navigation to Onboarding View
       if (context.mounted) {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-            builder: (context) => BlocProvider.value(
-              value: _loginBloc,
-              child: LoginView(),
-            ),
+            builder: (context) => const OnboardingView(),
           ),
         );
       }
