66916e377d71a4a861cb6e09a2d7f67a9f0e8407
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2025-02-13 13:19:23 +0545

    Fix login_bloc issues and improve state management

--- lib/features/auth/presentation/view_model/login/login_bloc.dart
+++ lib/features/auth/presentation/view_model/login/login_bloc.dart
@@ -1,10 +1,11 @@
+import 'package:bookit_flutter_project/core/common/snackbar/my_snackbar.dart';
 import 'package:bookit_flutter_project/features/auth/domain/use_case/login_use_case.dart';
-import 'package:bookit_flutter_project/view/home_view.dart';
+import 'package:bookit_flutter_project/features/auth/presentation/view_model/signup/register_bloc.dart';
+import 'package:bookit_flutter_project/features/dashboard/presentation/view/bottom_navigation/dashboard_view.dart';
+import 'package:bookit_flutter_project/features/dashboard/presentation/view_model/dashboard_cubit.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:bookit_flutter_project/core/common/snackbar/my_snackbar.dart';
-import 'package:bookit_flutter_project/features/auth/presentation/view_model/signup/register_bloc.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
@@ -12,12 +13,15 @@ part 'login_state.dart';
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final RegisterBloc _registerBloc;
   final LoginUseCase _loginUseCase;
+  final DashboardCubit _dashboardCubit;
 
   LoginBloc({
     required RegisterBloc registerBloc,
     required LoginUseCase loginUseCase,
+    required DashboardCubit dashboardCubit,
   })  : _registerBloc = registerBloc,
         _loginUseCase = loginUseCase,
+        _dashboardCubit = dashboardCubit,
         super(LoginState.initial()) {
     on<NavigateRegisterScreenEvent>(_onNavigateRegisterScreenEvent);
     on<NavigateHomeScreenEvent>(_onNavigateHomeScreenEvent);
@@ -46,7 +50,10 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     Navigator.pushReplacement(
       event.context,
       MaterialPageRoute(
-        builder: (context) => event.destination,
+        builder: (context) => BlocProvider.value(
+          value: _dashboardCubit,
+          child: event.destination,
+        ),
       ),
     );
   }
@@ -78,7 +85,7 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
         add(
           NavigateHomeScreenEvent(
             context: event.context,
-            destination: HomeView(),
+            destination: const DashboardView(),
           ),
         );
       },
