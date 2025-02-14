0196d5db01a1be3b6331d85e53f21179938f27a8
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2025-02-14 11:23:27 +0545

    Add new dashboard module

---
+++ lib/features/dashboard/presentation/view/bottom_navigation/dashboard_view.dart
@@ -0,0 +1,68 @@
+import 'package:bookit_flutter_project/features/dashboard/presentation/view_model/dashboard_cubit.dart';
+import 'package:bookit_flutter_project/features/dashboard/presentation/view_model/dashboard_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => DashboardCubit(),
+      child: Scaffold(
+        body: BlocBuilder<DashboardCubit, DashboardState>(
+          builder: (context, state) {
+            return state.pages[state.selectedIndex];
+          },
+        ),
+        bottomNavigationBar: BlocBuilder<DashboardCubit, DashboardState>(
+          builder: (context, state) {
+            return BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.white,
+              selectedItemColor: const Color(0xFF1E2751),
+              unselectedItemColor: Colors.grey[700],
+              iconSize: 27,
+              selectedFontSize: 13,
+              unselectedFontSize: 12,
+              currentIndex: state.selectedIndex,
+              onTap: (index) {
+                context.read<DashboardCubit>().onTabTapped(index);
+              },
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.menu_book),
+                  label: 'Explore',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.bookmark),
+                  label: 'Saved',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle),
+                  label: 'Profile',
+                ),
+              ],
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // cart functionality here
+          },
+          backgroundColor: const Color(0xFF1E2751),
+          child: const Icon(
+            Icons.shopping_cart,
+            size: 25,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      ),
+    );
+  }
+}
