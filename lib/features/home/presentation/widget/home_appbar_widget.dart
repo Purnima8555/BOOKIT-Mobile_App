5eb9bd6a44c9d2a08b6f8b6315410a00fd437ac6
Purnima8555 <110591365+Purnima8555@users.noreply.github.com>
2025-02-17 16:32:49 +0545

    Enhance UI and functionality in appbar widget

--- lib/features/home/presentation/widget/home_appbar_widget.dart
+++ lib/features/home/presentation/widget/home_appbar_widget.dart
@@ -1,4 +1,6 @@
+import 'package:bookit_flutter_project/features/auth/presentation/view_model/user/user_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BookAppBar extends StatelessWidget {
   final VoidCallback? onNotificationTap;
@@ -12,28 +14,177 @@ class BookAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverAppBar(
-      expandedHeight: 180,
-      floating: false,
-      pinned: true,
-      backgroundColor: const Color(0xFF1E2751),
-      elevation: 0,
-      flexibleSpace: FlexibleSpaceBar(
-        background: SafeArea(
-          child: Column(
-            children: [
-              // Top row with logo, welcome text, notifications, and profile
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
-                child: Row(
-                  children: [
-                    // App Logo
-                    Container(
+    return BlocBuilder<UserBloc, UserState>(
+      builder: (context, state) {
+        if (state.isLoading) {
+          return const SliverAppBar(
+            expandedHeight: 180,
+            floating: false,
+            pinned: true,
+            backgroundColor: Color(0xFF1E2751),
+            elevation: 0,
+            flexibleSpace: FlexibleSpaceBar(
+              background: SafeArea(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            ),
+          );
+        }
+
+        if (state.error.isNotEmpty) {
+          return SliverAppBar(
+            expandedHeight: 180,
+            floating: false,
+            pinned: true,
+            backgroundColor: const Color(0xFF1E2751),
+            elevation: 0,
+            flexibleSpace: FlexibleSpaceBar(
+              background: SafeArea(
+                child: Center(
+                  child: Text('Error: ${state.error}'),
+                ),
+              ),
+            ),
+          );
+        }
+
+        return SliverAppBar(
+          expandedHeight: 180,
+          floating: false,
+          pinned: true,
+          backgroundColor: const Color(0xFF1E2751),
+          elevation: 0,
+          flexibleSpace: FlexibleSpaceBar(
+            background: SafeArea(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 58,
+                          width: 58,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFF1E2751), // Updated color
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: const Color(
+                                  0xFF1E2751), // Updated border color
+                              width: 1,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                blurRadius: 8,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          child: ClipOval(
+                            child: Image.asset(
+                              'assets/images/logo_nobg.png', // Replace with your logo image path
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Welcome back,',
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                state.user?.username ?? 'John Doe',
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Stack(
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.notifications_outlined,
+                                  color: Colors.white),
+                              onPressed: onNotificationTap,
+                            ),
+                            Positioned(
+                              right: 8,
+                              top: 8,
+                              child: Container(
+                                padding: const EdgeInsets.all(4),
+                                decoration: const BoxDecoration(
+                                  color: Colors.red,
+                                  shape: BoxShape.circle,
+                                ),
+                                child: const Text(
+                                  '3',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        GestureDetector(
+                          onTap: onProfileTap,
+                          child: Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(color: Colors.white, width: 2),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  blurRadius: 8,
+                                  offset: const Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            child: ClipOval(
+                              child: state.user?.image != null
+                                  ? Image.network(
+                                      state.user!.image!,
+                                      fit: BoxFit.cover,
+                                    )
+                                  : Container(
+                                      color: Colors.grey[300],
+                                      child: const Icon(
+                                        Icons.person,
+                                        color: Colors.white,
+                                        size: 30,
+                                      ),
+                                    ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
+                    child: Container(
                       height: 50,
-                      width: 50,
                       decoration: BoxDecoration(
                         color: Colors.white,
-                        shape: BoxShape.circle,
+                        borderRadius: BorderRadius.circular(25),
                         boxShadow: [
                           BoxShadow(
                             color: Colors.black.withOpacity(0.1),
@@ -42,153 +193,51 @@ class BookAppBar extends StatelessWidget {
                           ),
                         ],
                       ),
-                      child: const Center(
-                        child: Icon(
-                          Icons.auto_stories,
-                          color: Color(0xFF6A1B9A),
-                          size: 30,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 12),
-                    // Welcome Text
-                    const Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Welcome back,',
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 12,
-                            ),
-                          ),
-                          Text(
-                            'John Doe',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    // Notification Bell
-                    Stack(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.notifications_outlined,
-                              color: Colors.white),
-                          onPressed: onNotificationTap,
-                        ),
-                        Positioned(
-                          right: 8,
-                          top: 8,
-                          child: Container(
-                            padding: const EdgeInsets.all(4),
-                            decoration: const BoxDecoration(
-                              color: Colors.red,
-                              shape: BoxShape.circle,
-                            ),
-                            child: const Text(
-                              '3',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 10,
-                                fontWeight: FontWeight.bold,
+                      child: TextField(
+                        onChanged: (value) {
+                          // Search text changed action
+                        },
+                        textAlign: TextAlign.start, // Text starts from the left
+                        decoration: InputDecoration(
+                          hintText: 'Search books, authors, genres...',
+                          hintStyle: TextStyle(color: Colors.grey[400]),
+                          prefixIcon: const Icon(Icons.search,
+                              color: Color(0xFF6A1B9A)),
+                          suffixIcon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                height: 24,
+                                width: 1,
+                                color: Colors.grey[300],
+                                margin:
+                                    const EdgeInsets.symmetric(vertical: 12),
                               ),
-                            ),
+                              IconButton(
+                                icon: const Icon(Icons.tune,
+                                    color: Color(0xFF6A1B9A)),
+                                onPressed: () {
+                                  // Show filter options
+                                },
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                    // Profile Picture
-                    GestureDetector(
-                      onTap: onProfileTap,
-                      child: Container(
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(color: Colors.white, width: 2),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.1),
-                              blurRadius: 8,
-                              offset: const Offset(0, 2),
-                            ),
-                          ],
-                        ),
-                        child: ClipOval(
-                          child: Container(
-                            color: Colors.grey[300],
-                            child: const Icon(
-                              Icons.person,
-                              color: Colors.white,
-                              size: 30,
-                            ),
+                          border: InputBorder.none,
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical:
+                                12, // Adjust vertical padding to center hint text
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              // Search Bar
-              Padding(
-                padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
-                child: Container(
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(25),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        blurRadius: 8,
-                        offset: const Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    onChanged: (value) {
-                      // Search text changed action
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Search books, authors, genres...',
-                      hintStyle: TextStyle(color: Colors.grey[400]),
-                      prefixIcon:
-                          const Icon(Icons.search, color: Color(0xFF6A1B9A)),
-                      suffixIcon: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Container(
-                            height: 24,
-                            width: 1,
-                            color: Colors.grey[300],
-                            margin: const EdgeInsets.symmetric(vertical: 12),
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.tune,
-                                color: Color(0xFF6A1B9A)),
-                            onPressed: () {
-                              // Show filter options
-                            },
-                          ),
-                        ],
-                      ),
-                      border: InputBorder.none,
-                      contentPadding:
-                          const EdgeInsets.symmetric(horizontal: 20),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
